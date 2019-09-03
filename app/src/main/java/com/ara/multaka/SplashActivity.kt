1090c635d68a075b67d4824ba935201e634aaa19
Ahmad-Mac <ahmad.rasheed1@yahoo.com>
2019-09-03 12:10:47 +0300

    splash screen is added with style of noction bar with full screen

---
+++ app/src/main/java/com/ara/multaka/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.ara.multaka
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+
+
+
+        val background = object : Thread() {
+
+            override fun run() {
+                Thread.sleep(2000)
+                val intent = Intent(applicationContext, MainActivity::class.java)
+                startActivity(intent)
+                finish();
+            }
+
+        }//End Thread
+
+        background.start()
+
+    }
+}
