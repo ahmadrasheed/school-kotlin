5fb826371251651478fc617fc376f1fd28b3e139
Ahmad-Mac <ahmad.rasheed1@yahoo.com>
2019-09-03 23:33:54 +0300

    splash screen is added with style of noction bar with full screen

--- app/src/main/java/com/ara/multaka/MainActivity.kt
+++ app/src/main/java/com/ara/multaka/MainActivity.kt
@@ -16,14 +16,25 @@ import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
 import java.io.IOException
 import android.widget.EditText
-import androidx.core.app.ComponentActivity
-import androidx.core.app.ComponentActivity.ExtraData
+
+
 import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.net.Uri
 import android.os.Environment
 import android.view.KeyEvent
 import android.webkit.*
+
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import kotlin.math.log
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var webWindow: WebView
@@ -34,16 +45,62 @@ class MainActivity : AppCompatActivity() {
 
         webWindow = findViewById(R.id.myWeb)
         webWindow.settings.javaScriptEnabled = true
+        webWindow.settings.setRenderPriority(WebSettings.RenderPriority.HIGH)
+        webWindow.settings.setAppCacheEnabled(true)
+        webWindow.settings.domStorageEnabled
+        webWindow.settings.saveFormData=true
+        webWindow.settings.enableSmoothTransition()
+
+
+
+
+
+
 
         webWindow.loadUrl("http://multaka.ahmadiraq.com")
-        webWindow.webViewClient = WebViewClient()
+
+        webWindow.webViewClient = object : WebViewClient() {
+
+            override fun onPageStarted(view: WebView, url: String, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                view.visibility =View.INVISIBLE
+                progressBar.visibility = View.VISIBLE
+            }
+
+            override fun onPageFinished(view: WebView, url: String) {
+                super.onPageFinished(view, url)
+                view.visibility =View.VISIBLE
+                progressBar.visibility = View.INVISIBLE
+            }
+
+
+
+
+
+
+
+        }
+
+
+
+
+        //webWindow.webViewClient =  WebViewClient()
         webWindow.webChromeClient = MyWebChromeClient()
-    }
+
+
+    } // End oncreate
 
     private var mFilePathCallback: ValueCallback<Array<Uri>>? = null
     private val FILECHOOSER_RESULTCODE = 1
 
+
+
     internal inner class MyWebChromeClient : WebChromeClient() {
+
+
+
+
+
         override fun onShowFileChooser(webView:WebView, filePathCallback: ValueCallback<Array<Uri>>, fileChooserParams:FileChooserParams):Boolean {
             mFilePathCallback = filePathCallback
             val intent = Intent(Intent.ACTION_GET_CONTENT)
@@ -51,8 +108,19 @@ class MainActivity : AppCompatActivity() {
             startActivityForResult(Intent.createChooser(intent, "Image Browser"), FILECHOOSER_RESULTCODE)
             return true
         }
+
+
+
+
+
+
     }
 
+
+
+
+
+//this code by ahmad to prevent close the app when navigation by presseing on keyback
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
@@ -61,6 +129,33 @@ class MainActivity : AppCompatActivity() {
             mFilePathCallback = null
         }
     }
+
+    private var keypressedtime=0
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+
+
+    if(webWindow.canGoBack()){
+        webWindow.goBack()
+        Log.d("xxxx","xxxxxxxxxx"+keypressedtime)
+    }
+        else {
+        keypressedtime++
+        if(keypressedtime>2) finish()
+   }
+
+        //return super.onKeyDown(keyCode, event)
+        return false
+
+
+    }
+
+
+
+
+
+
+
+
 }
 
     //End Class
